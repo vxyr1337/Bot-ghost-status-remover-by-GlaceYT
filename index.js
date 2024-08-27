c24ad1f8d013ac969d56e59b4e53887c76cede70
vxyr1337 <vxyr1337@icloud.com>
2024-08-27 12:31:24 +0200

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Listening to Spotify","Playing VALORANT"];
+const statusMessages = ["Listening to Spotify","Playing VALORANT","Playing Fortnite"];
 
 
 let currentIndex = 0;
